296a966ec9d8fe4bcf6b7cc2f8cc21349d230b4b
SerGreen <sergreeny@gmail.com>
2016-09-29 23:30:04 +0300

    quick save

---
+++ GeoQuiz/Controllers/QuizController.cs
@@ -0,0 +1,22 @@
+﻿using GeoQuiz.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeoQuiz.Controllers
+{
+    public class QuizController : Controller
+    {
+        List<QuestionAnswerPair> questions;
+
+        // GET: Quiz
+        public ActionResult Index(List<string> continents, List<int> allowedNonSovereigns)
+        {
+            
+
+            return View();
+        }
+    }
+}
